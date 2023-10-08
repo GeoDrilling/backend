a1d97ea7ac5f02ae9cd01e49e0bf806bf00543cc
Efim21216 <e.pashko@g.nsu.ru>
2023-10-08 17:32:29 +0700

    jwt

---
+++ geodrilling/src/main/java/ru/nsu/fit/geodrilling/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.geodrilling.repositories;
+
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nsu.fit.geodrilling.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+  Optional<User> findByEmail(String email);
+}
---
+++ geodrilling/src/test/java/ru/nsu/fit/geodrilling/GeodrillingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.geodrilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeodrillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
