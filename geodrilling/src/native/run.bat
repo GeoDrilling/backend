842026911e50b9af99a530eee6742facdab77b0c
Efim21216 <e.pashko@g.nsu.ru>
2023-10-31 15:50:40 +0700

    add instruction for calling C++ functions from java

--- geodrilling/src/native/run.bat
+++ geodrilling/src/native/run.bat
@@ -2,6 +2,5 @@ cd FakePicasso
 g++ PicassoLWD.FastSimulation.Native.cpp -c -std=c++20 -fPIC
 g++ PicassoLWD.FastSimulation.Native.o -shared -o ../PicassoLWD.FastSimulation.Native.dll
 cd ..
-g++ -fPIC -shared -o ../../native.dll -I"C:\Program Files\Java\jdk-11.0.16.1\include\win32" -I"C:\Program Files\Java\jdk-11.0.16.1\include" native.cpp ./PicassoLWD.FastSimulation.Native.dll
-move .\native.dll ..\..\
+g++ -fPIC -shared -o ../../native.dll -I "%JAVA_HOME%\include\win32" -I "%JAVA_HOME%\include" native.cpp ./PicassoLWD.FastSimulation.Native.dll
 move .\PicassoLWD.FastSimulation.Native.dll ..\..\
