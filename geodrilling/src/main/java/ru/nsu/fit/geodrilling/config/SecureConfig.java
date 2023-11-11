a71a1ea3736427b956080c4a36c77dcb51d08b0b
Efim21216 <e.pashko@g.nsu.ru>
2023-11-11 14:29:21 +0700

    fix deprecated http.cors()

--- geodrilling/src/main/java/ru/nsu/fit/geodrilling/config/SecureConfig.java
+++ geodrilling/src/main/java/ru/nsu/fit/geodrilling/config/SecureConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,9 +22,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.nsu.fit.geodrilling.filters.JwtFilter;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -50,9 +55,10 @@ public class SecureConfig {
             .addLogoutHandler(logoutHandler)
             .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
     );
-    http.cors();
+    http.cors(Customizer.withDefaults());
     return http.build();
   }
+
 
   @Bean
   public WebMvcConfigurer cors() {
