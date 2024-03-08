8daa90acc483264074c3330cbd913fd269d551e3
Efim21216 <e.pashko@g.nsu.ru>
2024-03-08 10:10:03 +0700

    add swagger

--- geodrilling/src/main/java/ru/nsu/fit/geodrilling/config/SecureConfig.java
+++ geodrilling/src/main/java/ru/nsu/fit/geodrilling/config/SecureConfig.java
@@ -22,74 +22,74 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.nsu.fit.geodrilling.filters.JwtFilter;
 
-import java.util.Arrays;
 
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecureConfig {
-  @Value("${application.security.allowed-origin}")
-  private String allowedOrigin;
-  private final UserDetailsService userDetailsService;
-  private final JwtFilter jwtFilter;
-  private final LogoutHandler logoutHandler;
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/**").permitAll()
-            .anyRequest().authenticated());
-    http.sessionManagement(session ->
-        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    http.authenticationProvider(authenticationProvider());
-    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    http.logout(logout ->
-        logout.logoutUrl("/auth/logout")
-            .addLogoutHandler(logoutHandler)
-            .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-    );
-    http.cors(Customizer.withDefaults());
-    return http.build();
-  }
+    @Value("${application.security.allowed-origin}")
+    private String allowedOrigin;
+    private final UserDetailsService userDetailsService;
+    private final JwtFilter jwtFilter;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.logout(logout ->
+                logout.logoutUrl("/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+        );
+        http.cors(Customizer.withDefaults());
+        return http.build();
+    }
 
 
-  @Bean
-  public WebMvcConfigurer cors() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins(allowedOrigin)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true);
-      }
-    };
-  }
+    @Bean
+    public WebMvcConfigurer cors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods(CorsConfiguration.ALL)
+                        .allowCredentials(true);
+            }
+        };
+    }
 
-  @Bean
-  public AuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService);
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
-  }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
-      throws Exception {
-    return config.getAuthenticationManager();
-  }
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
