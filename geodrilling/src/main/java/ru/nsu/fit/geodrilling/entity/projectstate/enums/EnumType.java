863a9e0e5aac960345ad88bcb760c5da9742c2ef
Efim21216 <e.pashko@g.nsu.ru>
2024-04-05 22:17:13 +0700

    исправил баг с enum свойствами

--- geodrilling/src/main/java/ru/nsu/fit/geodrilling/entity/projectstate/enums/EnumType.java
+++ geodrilling/src/main/java/ru/nsu/fit/geodrilling/entity/projectstate/enums/EnumType.java
@@ -3,7 +3,9 @@ package ru.nsu.fit.geodrilling.entity.projectstate.enums;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum EnumType {
-    ORIENTATION;
+    ORIENTATION,
+    SCALE,
+    BOOLEAN;
     @JsonValue
     public int toInt() {
         return ordinal();
